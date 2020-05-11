eb7d795e309d220f6caaf6e4edcb818877b17f86
skarning <sivertskarning@hotmail.com>
2020-05-11 14:05:22 +0200

    Initial commit

---
+++ settings.el
@@ -0,0 +1,99 @@
+
+;:Enables the use of melpa packages
+(package-initialize)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
+(elpy-enable)
+
+;: Enable visual-line-mode by default
+(visual-line-mode)
+
+;: Disabling menu-bar
+(menu-bar-mode -1)
+
+;: Disabling scroll-bar
+(toggle-scroll-bar -1)
+
+;: Disable tool-bar
+(tool-bar-mode -1)
+
+(package-initialize)
+(setq elpy-rpc-python-command "python3")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-buffer-choice nil)
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/org/skole.org" "~/Dropbox/org/privat.org")))
+ '(package-selected-packages
+   (quote
+    (gnu-elpa-keyring-update magit solarized-theme ac-ispell snazzy-theme plantuml-mode elpy drag-stuff auctex))))
+
+;: Sets shortcut for opening the org-agenda
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+;: Setting up date archiving in emacs
+(setq org-archive-location (concat "archive/archive-"
+                                   (format-time-string "%Y%m" (current-time))
+                                   ".org_archive::"))
+
+;: Export from org files to html
+(require 'ox-publish)
+(setq org-publish-project-alist
+      '(
+
+        ;; Configuration
+        ("org-notes"
+         :base-directory "~/org/"
+         :base-extension "org"
+         :publishing-directory "~/public_html/"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4             ; Just the default for this project.
+         :auto-preamble t
+         )
+
+      ))
+
+;: Configuration of the drag-stuff package
+(add-to-list 'load-path "/path/to/drag-stuff")
+(require 'drag-stuff)
+(drag-stuff-global-mode 1)
+(drag-stuff-define-keys)
+
+;: Loads auctex configuration
+(add-to-list 'load-path "~/.emacs.d/auctex")
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+;: Magit key-bindings
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;: Emacs theme
+;:(load-theme 'base16 t)
+;:(load-theme 'base16 t)
+
+;: Emacs starts automatically in full-screen mode
+(toggle-frame-fullscreen)
+
+(setq TeX-PDF-mode t)
+
+(require 'ess-r-mode)
+
+(add-to-list 'auto-mode-alist '("\\.R\\'" . r-mode))
+
+    (require 'company)
+    (setq tab-always-indent 'complete)
+    (setq company-idle-delay 0.1)
+    (global-company-mode)
+    (ess-toggle-underscore nil)
+    (with-eval-after-load 'ess
+      (setq ess-use-company t))
