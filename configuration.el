3e7ca04fea90157d7adf089bc88eeebacd4c9f33
skarning <sivertskarning@hotmail.com>
2025-09-01 21:21:17 +0200

    Add configuration from new computer

--- configuration.el
+++ configuration.el
@@ -1,57 +1,72 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-(require 'helm)
-(helm-mode 1)
-(global-set-key (kbd "M-x") 'helm-M-x)
+;; Bootstrap use-package (install it if not present)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents) ;; Update package list
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t) ;; Ensure packages are installed by default
 
-'(org-agenda-files '("~/tmp/workbook.org" "~/org/schedule.org"))
+;; Helm setup
+(use-package helm
+  :config
+  (helm-mode 1)
+  :bind (("M-x" . helm-M-x)))
 
+
+;; Magit setup
 (use-package magit
- :bind (("C-x g" . magit)))
+  :bind (("C-x g" . magit-status)))
+
+  (use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-solarized-dark t))
 
 
-;: Enable visual-line-mode by default
-(visual-line-mode)
+(use-package terraform-mode
+  :ensure t
+  :custom
+  (terraform-indent-level 2) ;; Set indentation to 2 spaces
+  (terraform-format-on-save t) ;; Auto-format with `terraform fmt` on save
+  :hook (terraform-mode . outline-minor-mode)) ;; Enable block folding
 
-;: Disabling menu-bar
+(use-package company
+  :ensure t
+  :config (global-company-mode t))
+
+(use-package company-terraform
+  :ensure t
+  :after (terraform-mode company)
+  :hook (terraform-mode . company-terraform-init))
+
+(use-package lsp-mode
+  :ensure t
+  :hook (terraform-mode . lsp-deferred)
+  :commands lsp)
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+;; UI tweaks
+(visual-line-mode 1)
 (menu-bar-mode -1)
-
-;: Disabling scroll-bar
 (toggle-scroll-bar -1)
-
-;: Disable tool-bar
 (tool-bar-mode -1)
-
-;: Automatically enable fullscreen
-(run-with-idle-timer 0.1 nil 'toggle-frame-fullscreen)
-
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-
-(setq org-todo-keywords
-      '((sequence "TODO"  "|" "IN PROGRESS" "CODE-REVIEW"  "|" "DONE")))
-
-(setq org-default-notes-file (concat org-directory "/quick-notes.org"))
-
+(run-with-idle-timer 0.1 nil 'toggle-frame-maximized)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/org/schedule.org"))
  '(package-selected-packages
-   '(helm wfnames use-package smart-tabs-mode popup plantuml-mode magit async)))
+   '(lsp-ui lsp-mode terraform-doc company-terraform company terraform-mode doom-themes magit helm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(use-package yaml-mode
-  :ensure t)
